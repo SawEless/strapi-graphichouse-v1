563f19d5dc1caf3fcec317bf79ffb21a0cf89de7
sailes <officialshailesh12@gmail.com>
2025-01-21 22:40:45 +0530

    done2y

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET', 'Jw7eK3nfk6PpLQWz3Y9vRA=='),
+      },
+    },
+  });
+  
