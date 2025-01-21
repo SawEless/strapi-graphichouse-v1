aa28761f08bfd566a4eb7542029628990cff3dc4
sailes <officialshailesh12@gmail.com>
2025-01-21 22:30:29 +0530

    done2

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'), // Specifies the host for your Strapi application
+  port: env.int('PORT', 1337),  // Specifies the port for your Strapi application
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['yourKeyA', 'yourKeyB']), // Sets the application keys for session middleware
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false), // Optional: Controls whether webhook payloads include related data
   },
 });
